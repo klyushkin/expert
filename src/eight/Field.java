fb87e9023b9468027b80d49de3932341f9cbb1e9
klyushkin <alexandr.klyushkin@gmail.com>
2015-03-10 11:15:47 +0300

    Start

--- src/eight/Field.java
+++ src/eight/Field.java
@@ -331,15 +331,7 @@ public class Field {
     }
 
     public void findResult(ArrayList<Integer> tmp) {
-       //test
-       //test
-       //test
-       //test
-       //test
-       //test
-       //test
-       //test
-        System.out.println("Test");
+     
     }
 
     public static void main(String[] args) {
